c8744a53073805e9755519582fb76a21c479dc74
Jordan Hall <jordan@hall05.co.uk>
2026-07-18 09:45:02 +0100

    Support current PSR Cache and cache invalidation

--- src/CachingObjectWrapper.php
+++ src/CachingObjectWrapper.php
@@ -9,17 +9,19 @@ class CachingObjectWrapper
     private $wrappedObject;
     private $cache;
     private $expiryInSeconds;
+    private $cacheNamespace;
 
-    public function __construct($objectToWrap, CacheItemPoolInterface $cacheItemPool, $expiryInSeconds)
+    public function __construct($objectToWrap, CacheItemPoolInterface $cacheItemPool, $expiryInSeconds, $cacheNamespace = null)
     {
         $this->wrappedObject = $objectToWrap;
         $this->cache = $cacheItemPool;
         $this->expiryInSeconds = $expiryInSeconds;
+        $this->cacheNamespace = $cacheNamespace === null ? get_class($objectToWrap) : (string) $cacheNamespace;
     }
 
     public function __call($name, $arguments)
     {
-        $cacheKey = sha1(get_class($this->wrappedObject).$name.serialize($arguments));
+        $cacheKey = $this->cacheKeyFor($name, $arguments);
 
         $cacheItem = $this->cache->getItem($cacheKey);
 
@@ -35,5 +37,39 @@ class CachingObjectWrapper
         $this->cache->save($cacheItem);
 
         return $response;
+    }
+
+    /**
+     * Return the cache key used for a method call.
+     */
+    public function cacheKeyFor($name, array $arguments = [])
+    {
+        return sha1($this->cacheNamespace.$name.serialize($arguments));
+    }
+
+    /**
+     * Remove one cached method call.
+     */
+    public function forgetCachedCall($name, array $arguments = [])
+    {
+        return $this->cache->deleteItem($this->cacheKeyFor($name, $arguments));
+    }
+
+    /**
+     * Remove and immediately recompute one cached method call.
+     */
+    public function refreshCachedCall($name, array $arguments = [])
+    {
+        $this->forgetCachedCall($name, $arguments);
+
+        return $this->__call($name, $arguments);
+    }
+
+    /**
+     * Return the object being wrapped.
+     */
+    public function getWrappedObject()
+    {
+        return $this->wrappedObject;
     }
 }
